f1fe471430a022d38e8b5d7106ef4d793bd90a46
Caio <caiodib10@gmail.com>
2026-05-27 15:37:11 -0300

    feat: configurando os relacionamento nos models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //
+
+    protected $fillable = ['user_id', 'name'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
+
 }
--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Expense extends Model
 {
     //
+    protected $fillable = ['user_id', 'category_id', 'description', 'amount', 'date'];
+
+
+   public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
+    
+
